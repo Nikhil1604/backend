260681be5f01f6866b2d7da57447fb15d27a197b
nikthewanted <77376892+nikthewanted@users.noreply.github.com>
2023-11-25 23:00:37 +0530

    started

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import { Box } from '@mui/material'
+
+const Welcome = () => {
+  return (
+    <Box>
+        <>
+        <Typography  varient="h1">Coated Lens</Typography>
+        </>
+        <>
+        
+        </>
+    </Box>
+  )
+}
+
+export default Welcome
